5d00b815475f918c8062037fdc6e84385fd68f67
rc2pc2 <petriccarcc@gmail.com>
2023-08-31 12:33:45 +0200

    added simple and horriblle but functional pagination to posts index

--- src/pages/Posts.vue
+++ src/pages/Posts.vue
@@ -3,6 +3,14 @@
         <h1 class="main-title">
             Latest Posts:
         </h1>
+        <nav class="pagination">
+            <li class="prev" @click="prevPage" v-if="prevPageUrl">
+                Previous
+            </li>
+            <li class="next" @click="nextPage" v-if="nextPageUrl">
+                Next
+            </li>
+        </nav>
         <div class="posts">
             <SinglePost class="single-post w-50" v-for="post in posts" :post='post'
                 @click="$router.push({ name: 'posts.show', params: { slug: post.slug} })" />
@@ -22,24 +30,38 @@ export default {
     data() {
         return {
             posts : [],
+            nextPageUrl : '',
+            currentPageNo: '',
+            prevPageUrl : '',
             apiUrl : 'http://127.0.0.1:8000/api/posts'
         }
     },
 
     methods: {
-        getPosts(){
+        getPosts(apiUrl = this.apiUrl){
             // recuper i miei post e popolo la variabile posts
-            axios.get(this.apiUrl, {
-                params: {}
-            })
+            axios.get(apiUrl)
             .then((response) => {
-                console.log(response.data.results.data)
+                console.log(response)
                 this.posts = response.data.results.data;
+                this.nextPageUrl = response.data.results.next_page_url;
+                this.prevPageUrl = response.data.results.prev_page_url;
             })
             .catch(function (error) {
                 console.log(error);
             })
-        }
+        },
+
+        nextPage(){
+            // alert('next page');
+            this.getPosts(this.nextPageUrl);
+        },
+
+        prevPage(){
+            // alert('prev page');
+            this.getPosts(this.prevPageUrl);
+        },
+
     },
 
     created(){
@@ -57,5 +79,25 @@ export default {
         justify-content: center;
         align-items: center;
         flex-wrap: wrap;
+    }
+
+    nav.pagination{
+        width: 100%;
+        padding: 1rem;
+        display: flex;
+        // justify-content: space-between;
+        align-items: center;
+        background-color: black;
+        list-style-type: none;
+
+        li{
+            padding: 1rem;
+            background-color: blue;
+            color: white;
+
+            &.next{
+                margin-left: auto;
+            }
+        }
     }
 </style>
